8bfeb5bd5b4180b7638c6e9fd5598b788d40c843
harukaR <awwrate1218@gmail.com>
2023-04-06 22:55:52 +0900

    Profileコンポーネントを

--- src/components/Layout/Profile.tsx
+++ src/components/Layout/Profile.tsx
@@ -7,25 +7,27 @@ export const Profile = ()=>{
             <div className={styles.profileWrap}>
                 <Image src='/avatar.png' width={100} height={100} className={styles.profileThm} alt='アバターの画像'/>
                 <p className={styles.profileName}>haruka</p>
-                <dl className={styles.profileSkill}>
-                    <dt>skills</dt>
-                    <dd>
-                        <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/HTML.svg' width={30} height={30} alt="htmlのアイコン"/>Html</p>
-                        <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Sass.svg' width={30} height={30} alt="sassのアイコン"/>Sass</p>
-                        <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/JavaScript.svg' width={30} height={30} alt="JavaScriptのアイコン"/>Javascript</p>
-                        <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Photoshop.svg' width={30} height={30} alt="Photoshopのアイコン"/>Photoshop</p>
-                        <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Illustrator.svg' width={30} height={30} alt="Illustratorのアイコン"/>Illustrator</p>
-                    </dd>
-                </dl>
-                <dl className={`${styles.profileSkill} ${styles.haveMargin}`}>
-                    <dt>learning</dt>
-                    <dd>
-                        <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/React-Light.svg' width={30} height={30} alt="Reactのアイコン"/>React</p>
-                        <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/TypeScript.svg' width={30} height={30} alt="Typescriptのアイコン"/>Typescript</p>
-                        <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/NextJS-Light.svg' width={30} height={30} alt="Next.jsのアイコン"/>Next.js</p>
-                        
-                    </dd>
-                </dl>
+                <div className={styles.profileSkillMap}>
+                    <dl className={styles.profileSkill}>
+                        <dt>Skills</dt>
+                        <dd>
+                            <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/HTML.svg' width={25} height={25} alt="htmlのアイコン"/><span className={styles.skillName}>Html</span></p>
+                            <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Sass.svg' width={25} height={25} alt="sassのアイコン"/><span className={styles.skillName}>Sass</span></p>
+                            <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/JavaScript.svg' width={25} height={25} alt="JavaScriptのアイコン"/><span className={styles.skillName}>Javascript</span></p>
+                            <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Photoshop.svg' width={25} height={25} alt="Photoshopのアイコン"/><span className={styles.skillName}>Photoshop</span></p>
+                            <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Illustrator.svg' width={25} height={25} alt="Illustratorのアイコン"/><span className={styles.skillName}>Illustrator</span></p>
+                        </dd>
+                    </dl>
+                    <dl className={`${styles.profileSkill} ${styles.haveMargin}`}>
+                        <dt>Learning</dt>
+                        <dd>
+                            <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/React-Light.svg' width={25} height={25} alt="Reactのアイコン"/><span className={styles.skillName}>React</span></p>
+                            <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/TypeScript.svg' width={25} height={25} alt="Typescriptのアイコン"/><span className={styles.skillName}>Typescript</span></p>
+                            <p><Image src='https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/NextJS-Light.svg' width={25} height={25} alt="Next.jsのアイコン"/><span className={styles.skillName}>Next.js</span></p>
+                            
+                        </dd>
+                    </dl>
+                </div>
                 {/* <dl className={styles.profileItem}>
                     <dt>趣味</dt>
                     <dd>ゲーム   /   読書</dd>
@@ -40,10 +42,11 @@ export const Profile = ()=>{
                 </dl>
                 <dl className={styles.profileItem}>
                     <dt>経歴</dt>
-                    <dd>2020年:現在の会社にアルバイト入社(レポーター)</dd>
-                    <dd>2021年:正社員登用(コーダーに転身)</dd>
-                    <dd>2023年:フロントエンドエンジニアに転身</dd>
+                    <dd>2525年:現在の会社にアルバイト入社(レポーター)</dd>
+                    <dd>2521年:正社員登用(コーダーに転身)</dd>
+                    <dd>2523年:フロントエンドエンジニアに転身</dd>
                 </dl> */}
+                
 
             </div>
 
