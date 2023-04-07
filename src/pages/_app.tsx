4fb1b228201ad07daa60d5cb0545304e1deffaef
harukaR <awwrate1218@gmail.com>
2023-04-08 02:36:28 +0900

    recoil適応した

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import ThemeProvider from 'components/ThemeProvider/ThemeProvider';
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from "recoil";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return( 
+    <RecoilRoot>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+    </RecoilRoot>
+
+        )
 }
