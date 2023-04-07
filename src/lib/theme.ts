1985cd925ad6492f275eb7b6d19526240748ee09
harukaR <awwrate1218@gmail.com>
2023-04-08 02:35:43 +0900

    ダークモード用の状態管理用の関数を追加

---
+++ src/lib/theme.ts
@@ -0,0 +1,26 @@
+import { atom, useRecoilState, useSetRecoilState } from "recoil"
+//テーマの状態で使う型を定義
+export type Theme = "light" | "dark";
+
+//状態管理
+const themeState = atom<Theme> ({
+    key:"themeState",
+    default:'light',
+});
+
+// 状態管理しているもの更新(のみ)できるようにしてかつ他のコンポーネントに渡せるようにする
+export const useSetTheme = ()=> useSetRecoilState(themeState);
+
+export const useTheme = ()=>{
+    //取得と更新ができるように定義
+    const [theme,setTheme] = useRecoilState(themeState);
+    const toggleTheme = ()=>{
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(theme);
+        window.localStorage.setItem("theme",newTheme);
+        const root = window.document.documentElement;
+        root.setAttribute("data-theme", newTheme);
+    }
+    return { theme, toggleTheme };
+
+}
