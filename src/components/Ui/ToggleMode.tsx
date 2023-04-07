9401bb9d33525a802ee7539ef15989fda11a8e32
harukaR <awwrate1218@gmail.com>
2023-04-08 02:36:08 +0900

    スタイルを諸々調整

--- src/components/Ui/ToggleMode.tsx
+++ src/components/Ui/ToggleMode.tsx
@@ -2,12 +2,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSun } from '@fortawesome/free-solid-svg-icons'
 import { faMoon } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
+import { useTheme } from "lib/theme";
 
 import styles from 'components/Ui/toggleMode.module.scss'
 
 import React from 'react'
 
-
+const { toggleTheme } = useTheme();
 export const ToggleMode = () => {
     const [mode,setMode] = useState(true);
     const handleMode =()=>{
@@ -15,7 +16,7 @@ export const ToggleMode = () => {
       console.log(mode)
     }
   return (
-    <div onClick={handleMode}>
+    <div onClick={toggleTheme}>
         {mode ? <FontAwesomeIcon icon={faSun} className={styles.modeIcon}  /> : <FontAwesomeIcon icon={faMoon} className={styles.modeIcon} />}
     </div>
   )
